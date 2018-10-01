bb1057e7d32a0695d4e768ac0707bc020227485f
Paul Nunez <paulnunezm@gmail.com>
2018-09-30 20:39:34 -0400

    edit: optimized exercise

--- 1.arrays_and_strings/q_1/q_1.4_palindromePermutation.kt
+++ 1.arrays_and_strings/q_1/q_1.4_palindromePermutation.kt
@@ -8,34 +8,32 @@ fun main(args: Array<String>) {
     print(result)
 }
 
+
+/**
+ *  For it to be a palindrome it must be a string with odd length of characters
+ *  and it must be only one odd character count
+ *
+ *  Optimized: do both operations at the same iteration
+ */
 fun isPalindromePermutation(input: String): Boolean {
     val lowerCasedInput = input.toLowerCase()
-
     val characterCounter = Array(128, { 0 }) // ASCII character list
     var numberOfCharacters = 0
 
+    var oddCount = 0
     for (i in 0 until lowerCasedInput.length) {
         if (lowerCasedInput[i] != ' ') {
-            numberOfCharacters++
             val char = lowerCasedInput[i].toInt()
-            characterCounter[char] += 1
-        }
-    }
+            numberOfCharacters++
+            characterCounter[char] ++
 
-    // for it to be a palindrome it must be a string with odd length of characters
-    if (numberOfCharacters % 2 == 0) return false
-
-    // it must be only one odd character count
-    var oddCount = 0
-    for (i in characterCounter) {
-        val characterCount = characterCounter[i]
-        if (characterCount % 2 != 0) {
-            if (oddCount > 0) {
-                return true
-            } else {
+            if (characterCounter[char] % 2 != 0) {
                 oddCount++
+            } else {
+                oddCount--
             }
         }
     }
-    return true
+    if (numberOfCharacters % 2 == 0) return false
+    return  oddCount <= 1
 }
