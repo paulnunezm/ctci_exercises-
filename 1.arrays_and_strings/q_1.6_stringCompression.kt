4ddf6221d0c94954be125dc567ee435583649b4d
Paul Nunez <paulnunezm@gmail.com>
2018-10-05 16:14:29 -0400

    add: stringCompression 1.1.9 question answer

--- 1.arrays_and_strings/q_1.6_stringCompression.kt
+++ 1.arrays_and_strings/q_1.6_stringCompression.kt
@@ -1,8 +1,28 @@
 fun main(args: Array<String>) {
-
+    println(getCompressedString("aabcccccaaabv"))
 }
 
 fun getCompressedString(s: String): String {
+    var characterCounter = 1
+    val stringBuilder = StringBuilder()
 
-    return ""
+    for (i in 0 until s.length) {
+        if (i < s.length - 1) {
+            if (s[i] == s[i + 1]) {
+                characterCounter++
+            } else {
+                stringBuilder.append("${s[i]}$characterCounter")
+                characterCounter = 1
+            }
+        } else {
+            stringBuilder.append("${s[i]}$characterCounter")
+        }
+    }
+
+    val compressedString = stringBuilder.toString()
+    return if (s.length < compressedString.length) {
+        s
+    } else {
+        compressedString
+    }
 }
